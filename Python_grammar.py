0f7a104e435bd46e27a1501241cd133c44c20ef6
Leesuhuck <globalmainpcb@gmail.com>
2020-11-01 17:33:43 +0900

    Add Text File [Python_grammar.py]

---
+++ Python_grammar.py
@@ -0,0 +1,17 @@
+a_string= '변수생성'
+a_number =2
+b_float =3.12
+print(a_number+b_float)
+aa_boolean = True
+ab_boolean = False
+a_none = None
+
+#리스트
+days = ["mon","Tue","Wed","Thur","Fri"]
+#[인덱스]찾기
+print("mon" in days)
+print(days[3])
+print(days)
+days.append("Sat")
+days.reverse()
+print(days)
